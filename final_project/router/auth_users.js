e5fd79003e03c842c82a704ccbea9ee175141bde
Rian <rhiannon.illustration@gmail.com>
2023-06-30 13:23:24 -0400

    task 7, 8, and 9 updated
    Signed-off-by: Rian <rhiannon.illustration@gmail.com>

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -58,50 +58,28 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   const username = req.body.username;
   const isbn = parseInt(req.params.isbn); 
   const book = books[isbn];
-
-  /* pseudocode: check request body for username
-  if exists,check if book with isbn exists
-  if exists modify reviews and add username: "review here" to review {}
-    book[req.body.reviews] ={
-        "reviews": req.body.username
-    }
-
-  */
-
-
-  if (username){
+  if (username) {
     if (isbn < 1 || isbn > 10) {
-      res.send("Not a valid month number")
-    }
-
-  else {
-    // check if review exists:
-    if (book.reviews[username]) {
-      let review = req.body.reviews;
-
-      if(review) {
-        book.reviews[username] = review
-        res.send(`review exists`);
+      res.send("Not a valid month number");
+    } else {
+      // Check if review exists
+      if (book.reviews && book.reviews[username]) {
+        let review = req.body.reviews;
+        book.reviews[username] = review;
+        res.send(`Review updated`);
+      } else {
+        let review = req.body.reviews;
+        book.reviews = {
+          [username]: review
+        };
+        res.send(`Review added`);
       }
-
-    else {
-      book.reviews = {
-        [req.body.username]: req.body.review
-      }
-      res.send(`review added`);
-      //book[req.body.reviews]={
-      //  "username": req.body.reviews
-      //}
-    }
-    
-    }
-  }
-
       return res.status(200).send(`Book review with ISBN ${isbn} by user ${username} updated.`);
-    } 
-  else {
-    return res.status(404).json({ message: "user doesn't exist" });
+    }
+  } else {
+    return res.status(404).json({ message: "User doesn't exist" });
   }
+
 });
 
 
@@ -110,7 +88,7 @@ regd_users.delete("/auth/review/:isbn", (req, res) => {
   const password = req.body.password;
   const isbn = parseInt(req.params.isbn); 
 
-    if (authenticatedUser(username, password)) {
+    if (username) {
       const book = books[isbn];
 
       if (book && book.reviews && book.reviews[username]) {
